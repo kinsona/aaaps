0ee54f9749dcfe7abc25c3b89a85bff680cb83a3
Adam Kinson <adam.kinson@gmail.com>
2016-11-20 11:44:03 -0500

    setup rack-cors

--- config/application.rb
+++ config/application.rb
@@ -23,14 +23,21 @@ module LadyProblems
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.action_dispatch.default_headers = {
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    # config.action_dispatch.default_headers = {
         # 'Access-Control-Allow-Origin' => 'http://lady-problems-zoe.s3-website-us-east-1.amazonaws.com',
         # 'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
         # 'Access-Control-Allow-Headers' => "Content-Type";
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Allow-Methods' => 'POST, PUT, DELETE, GET, OPTIONS',
-        'Access-Control-Request-Method' => '*',
-        'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      }
-  end
+  #       'Access-Control-Allow-Origin' => '*',
+  #       'Access-Control-Allow-Methods' => 'POST, PUT, DELETE, GET, OPTIONS',
+  #       'Access-Control-Request-Method' => '*',
+  #       'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  #     }
+  # end
 end
